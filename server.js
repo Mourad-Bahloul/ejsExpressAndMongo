4698575266041e862d358f0d6e49eb5dc28816fa
Mourad BAHLOUL <mourad_bahloul@parts-advisor.com>
2020-08-03 08:32:20 +0100

    setting up express and ejs

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + "/views")
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started')
+})
