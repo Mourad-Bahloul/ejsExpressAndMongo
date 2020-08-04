821bbf17ea178950606650ce01760b5f333f0f41
Mourad BAHLOUL <mourad_bahloul@parts-advisor.com>
2020-08-04 10:05:09 +0100

    adding authors route

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const expressLayouts = require('express-ejs-layouts')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + "/views")
 app.set('layout', 'layouts/layout')
-
-app.use(expressLayouts)
 app.use(express.static('public'))
+
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts)
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+})    
 const db = mongoose.connection
 db.on('error', (error) => { console.error(error) })
 db.once('open', () => console.log('Database connected'))
 
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
+
+const authorRouter = require('./routes/authors')
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started')
