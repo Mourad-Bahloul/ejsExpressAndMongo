542b34c9caa6bc6487c5010a5ea3676cb97aca6a
Mourad BAHLOUL <mourad_bahloul@parts-advisor.com>
2020-08-04 09:35:46 +0100

    adding of mongoose

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require('express')
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 require('dotenv').config()
+const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
@@ -9,6 +12,15 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', (error) => { console.error(error) })
+db.once('open', () => console.log('Database connected'))
 
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
